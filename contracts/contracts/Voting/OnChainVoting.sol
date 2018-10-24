c22271e9d60596e8d3fa05bae4545530860fd8d0
Stuart Hunter <stuarth323@gmail.com>
2018-10-24 14:07:38 -0400

    initial implementation done

--- contracts/contracts/Voting/OnChainVoting.sol
+++ contracts/contracts/Voting/OnChainVoting.sol
@@ -7,15 +7,15 @@ import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 contract OnChainVoting is Ownable { 
 
     // Will be a users salt hashed;
-    mapping(bytes32 => uint256) vote;
-    uint256 voteCount = 0;
+    mapping(bytes32 => uint256) votes;
+    uint256 votesCount = 0;
 
     // Will be token holder address to Encrypted(salt ++ vote) 
     mapping(address => bytes) submissions;
     uint256 submissionsCount = 0;
 
     uint256 result;
-    uint endTime;
+    uint256 endTime;
 
     // Security Token
     IERC20 public securityToken;
@@ -23,11 +23,12 @@ contract OnChainVoting is Ownable {
     constructor(
         IERC20 _securityToken, 
         address _issuer, 
-        uint _endTime
+        uint256 _endTime
     ) 
         public 
     {
         require(_securityToken != address(0), "Token address cannot be zero.");
+        require(block.timestamp < _endTime);
 
         securityToken = _securityToken;
         endTime = _endTime;
@@ -39,11 +40,7 @@ contract OnChainVoting is Ownable {
 	* @dev function for every token holder to submit their vote
 	* @param _encVote Encrypted userSalt concatenated with their vote, encrypted with votingAdmin's public key
     **********************************************/
-    function placeVote(
-        bytes _encVote
-    ) 
-        public  
-    {
+    function placeVote(bytes _encVote) public  {
         require(securityToken.balanceOf(msg.sender) > 0, "Token Balance must be greater than 0");
         require(_encVote.length > 0, "Must submit a valid vote");
         require(block.timestamp < endTime, "Can only place vote before time is up");
@@ -52,21 +49,38 @@ contract OnChainVoting is Ownable {
         submissionsCount += 1;
     }
 
-
-    /**********************************************
+/**********************************************
 	* @dev function for votingAdmin to submit every vote
-	* @param _userSalt Hash of users decrypted salt
-    * @param _userVote Plaintext value of users vote
+	* @param _usersSaltHash Array of hashes of users decrypted salt
+    * @param _usersVote Array of plaintext value of users vote
     **********************************************/
-    function submitVote(
-        bytes32 _userSalt, 
-        uint256 _userVote
+    function submitUserVotes(
+        bytes32[] _usersSaltHash, 
+        uint256[] _usersVote
     )
         public 
         onlyOwner
     {
-        vote[_userSalt] = _userVote; 
-        voteCount += 1;
+        require(_usersSaltHash.length == _usersVote.length, "Arrays must be same length");
+        for (uint256 i = 0; i < _usersSaltHash.length; i++) {
+            submitVote(_usersSaltHash[i], _usersVote[i]);
+        }
+        votesCount += _usersSaltHash.length;
+    }
+
+    /**********************************************
+	* @dev function for votingAdmin to submit every vote
+	* @param _userSaltHash Hash of users decrypted salt
+    * @param _userVote Plaintext value of users vote
+    **********************************************/
+    function submitVote(
+        bytes32 _userSaltHash, 
+        uint256 _userVote
+    )
+        private
+    {
+        require(votes[_userSaltHash] == 0, "Cannot resubmit a users vote");
+        votes[_userSaltHash] = _userVote; 
     }
 
 
@@ -80,7 +94,7 @@ contract OnChainVoting is Ownable {
         public 
         onlyOwner 
     {
-        require(submissionsCount == voteCount, "Must have correct number of submissions tallied ot votes");
+        require(submissionsCount == votesCount, "Must have correct number of submissions tallied to votes");
         require(result == 0, "May only submit the result once");
         require(block.timestamp > endTime, "Can only finalize once the voting has closed");
         result = _result;

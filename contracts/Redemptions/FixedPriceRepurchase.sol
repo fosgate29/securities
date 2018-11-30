5545f34c6515093f7a72e8a399ef1ae6a20bd852
Alice Henshaw <henshawalice@gmail.com>
2018-11-30 16:32:56 -0500

    changes to tender offer after new info came from Brian

--- contracts/Redemptions/FixedPriceRepurchase.sol
+++ contracts/Redemptions/FixedPriceRepurchase.sol
@@ -6,43 +6,91 @@ import "./RedeemableToken.sol";
 contract FixedPriceRepurchase is Ownable {
     using SafeMath for uint256;
 
-    uint256 offerPerSecurity;
     IERC20 paymentToken;
     RedeemableToken securityToken;
     address paymentOwner;
 
+    uint256 offerPerSecurity;
     uint256 totalToRepurchase;
-    uint256 numberRepurchased;
     uint256 saleEndTime;
 
+    struct HolderTender {
+        uint256[] tenderAmounts;
+        uint256 totalTendered;
+    }
+
+    mapping(address => HolderTender) tenders;
+    address[] orderedHolders;
+
     constructor(
-        uint256 _offerPerSecurity,
+        uint256 _amountPerSecurity,
         IERC20 _paymentToken,
         RedeemableToken _securityToken,
-        address _paymentOwner,
-        uint256 _totalToRepurchase,
-        uint256 _saleEndTime
+        address _paymentOrigin,
+        uint256 _totalNoToRepurchase,
+        uint256 _offerEndTime
     ) public {
-        offerPerSecurity = _offerPerSecurity;
+        amountPerSecurity = _amountPerSecurity;
         paymentToken = _paymentToken;
         securityToken = _securityToken;
-        paymentOwner = _paymentOwner;
-        totalToRepurchase = _totalToRepurchase;
-        saleEndTime = _saleEndTime;
+        paymentOrigin = _paymentOrigin;
+        totalNoToRepurchase = _totalNoToRepurchase;
+        offerEndTime = _offerEndTime;
     }
 
-    function updateSaleEndTime(uint256 _newSaleEnd) external onlyOwner {
-        require(_newSaleEnd >= now, "New sale end time cannot be in the past");
-        saleEndTime = _newSaleEnd;
+    function updateOfferEndTime(uint256 _newOfferEnd) external onlyOwner {
+        require(_newOfferEnd >= now, "New sale end time cannot be in the past");
+        offerEndTime = _newOfferEnd;
     }
 
-    function confirmRepurchase(uint256 _numberOfSecurities) external {
-        require(securityToken.balanceOf(msg.sender) >= _numberOfSecurities, "sender does not own enough securities");
-        require(numberRepurchased < totalToRepurchase);
+    function optInToTender(uint256 _numberToTender) external {
+        // create a reference so the changes update the mapping
+        HolderTender storage holderTender = tenders[msg.sender];
 
-        uint256 paymentOwed = _numberOfSecurities * offerPerSecurity;
-        paymentToken.transferFrom(paymentOwner, msg.sender, paymentOwed);
-        securityToken.redeemPartialTokens(msg.sender, _numberOfSecurities);
+        // calculate new total tender from holder
+        uint256 newTotalTendered = holderTender.totalTendered + _numberToTender;
+
+        require(securityToken.balanceOf(msg.sender) >= newTotalTendered, "sender does not own enough securities");
+        require(
+            securityToken.allowance(msg.sender, address(this)) >= newTotalTendered,
+            "holder has not approved contract to control securities"
+        );
+
+        // update holder's tender to reflect this addition
+        holderTender.totalTendered = newTotalTendered;
+        holderTender.tenderAmounts.push(_numberToTender);
+
+        // transfer these tokens from the holder to this contract
+        securityToken.transferFrom(msg.sender, address(this), _numberToTender);
+    }
+
+    function optOutOfTender(uint256 _numberToRemove) external {
+        // create a reference so the changes update the mapping
+        HolderTender storage holderTender = tenders[msg.sender];
+
+        require(_numberToRemove > 0, "must provide a number of securities to opt out");
+        require(holderTender.totalTendered >= _numberToRemove, "sender has not tendered enough securities");
+
+        // update holder's tender to reflect this addition
+        uint256 remainingToRemove = _numberToRemove;
+        uint256 currentTender;
+        uint256 currentIndex = holderTender.tenderAmounts.length-1;
+        uint256 toRemove;
+
+        while (remainingToRemove > 0) {
+            // get amount of final array element
+            currentTender = holderTender.tenderAmounts[currentIndex];
+
+            toRemove = min256(currentTender, remainingToRemove);
+            remainingToRemove -= toRemove;
+            holderTender.tenderAmounts[currentIndex] -= toRemove;
+            currentIndex -= 1;
+        }
+
+        holderTender.totalTendered -= _numberToRemove;
+
+        // transfer these tokens from the holder to this contract
+        securityToken.transfer(msg.sender, _numberToTender);
     }
 
     // TODO do we need to be able to specify who is entitled to a repurchase? Or if they start one are all holders entitled to one?

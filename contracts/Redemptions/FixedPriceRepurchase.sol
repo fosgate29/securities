01a12b4392acc13a62b48d32cec43e7ded3beeec
Alice Henshaw <henshawalice@gmail.com>
2018-12-03 10:56:47 -0500

    New tender offering contract

--- contracts/Redemptions/FixedPriceRepurchase.sol
+++ contracts/Redemptions/FixedPriceRepurchase.sol
@@ -8,68 +8,94 @@ contract FixedPriceRepurchase is Ownable {
 
     IERC20 paymentToken;
     RedeemableToken securityToken;
-    address paymentOwner;
+    address issuer;
 
-    uint256 offerPerSecurity;
+    uint256 paymentPerSecurity;
     uint256 totalToRepurchase;
     uint256 saleEndTime;
 
+    uint256 nextTenderToAssess;
+    uint256 totalRepurchased;
+    uint256 overallTotalTendered;
+
     struct HolderTender {
         uint256[] tenderAmounts;
-        uint256 totalTendered;
+        uint256 holderTotalTendered;
+        uint256 nextToProcess;
     }
 
     mapping(address => HolderTender) tenders;
     address[] orderedHolders;
 
+    modifier isBeforeEndTime {
+        require(now < offerEndTime, "The tender offer has expired");
+        _;
+    }
+
+    modifier isAfterEndTime {
+        require(now >= offerEndTime, "The tender offer has not yet ended");
+        _;
+    }
+
     constructor(
-        uint256 _amountPerSecurity,
+        uint256 _paymentPerSecurity,
         IERC20 _paymentToken,
         RedeemableToken _securityToken,
-        address _paymentOrigin,
-        uint256 _totalNoToRepurchase,
+        address _issuer,
+        uint256 _totalToRepurchase,
+        uint256 _offerPerSecurity,
         uint256 _offerEndTime
     ) public {
-        amountPerSecurity = _amountPerSecurity;
+        require (
+            _paymentToken.allowance(_issuer, address(this)) >= _totalNoToRepurchase.mul(_offerPerSecurity),
+            "The contract does not have access to enough payment tokens"
+        );
+        require(_totalNoToRepurchase <= _securityToken.totalSupply(), "Total to repurchase is larger than total token supply");
+
+        paymentPerSecurity = _paymentPerSecurity;
         paymentToken = _paymentToken;
         securityToken = _securityToken;
-        paymentOrigin = _paymentOrigin;
-        totalNoToRepurchase = _totalNoToRepurchase;
+        issuer = _issuer;
+        totalToRepurchase = _totalToRepurchase;
         offerEndTime = _offerEndTime;
     }
 
-    function updateOfferEndTime(uint256 _newOfferEnd) external onlyOwner {
+    function updateOfferEndTime(uint256 _newOfferEnd) external onlyOwner isBeforeEndTime {
         require(_newOfferEnd >= now, "New sale end time cannot be in the past");
         offerEndTime = _newOfferEnd;
     }
 
-    function optInToTender(uint256 _numberToTender) external {
+    function optInToTender(uint256 _numberToTender) external isBeforeEndTime {
+        require(_numberToTender > 0, "Must provide a number of securities to opt in");
         // create a reference so the changes update the mapping
         HolderTender storage holderTender = tenders[msg.sender];
 
         // calculate new total tender from holder
-        uint256 newTotalTendered = holderTender.totalTendered + _numberToTender;
+        uint256 newholderTotalTendered = holderTender.holderTotalTendered + _numberToTender;
 
-        require(securityToken.balanceOf(msg.sender) >= newTotalTendered, "sender does not own enough securities");
+        require(securityToken.balanceOf(msg.sender) >= newholderTotalTendered, "Sender does not own enough securities");
         require(
-            securityToken.allowance(msg.sender, address(this)) >= newTotalTendered,
-            "holder has not approved contract to control securities"
+            securityToken.allowance(msg.sender, address(this)) >= newholderTotalTendered,
+            "Holder has not approved contract to control securities"
         );
 
         // update holder's tender to reflect this addition
-        holderTender.totalTendered = newTotalTendered;
+        holderTender.holderTotalTendered = newholderTotalTendered;
         holderTender.tenderAmounts.push(_numberToTender);
+
+        overallTotalTendered += _numberToTender;
 
         // transfer these tokens from the holder to this contract
         securityToken.transferFrom(msg.sender, address(this), _numberToTender);
     }
 
-    function optOutOfTender(uint256 _numberToRemove) external {
+    function optOutOfTender(uint256 _numberToRemove) external isBeforeEndTime {
+        require(_numberToRemove > 0, "Must provide a number of securities to opt out");
+
         // create a reference so the changes update the mapping
         HolderTender storage holderTender = tenders[msg.sender];
 
-        require(_numberToRemove > 0, "must provide a number of securities to opt out");
-        require(holderTender.totalTendered >= _numberToRemove, "sender has not tendered enough securities");
+        require(holderTender.holderTotalTendered >= _numberToRemove, "Sender has not tendered enough securities");
 
         // update holder's tender to reflect this addition
         uint256 remainingToRemove = _numberToRemove;
@@ -87,10 +113,60 @@ contract FixedPriceRepurchase is Ownable {
             currentIndex -= 1;
         }
 
-        holderTender.totalTendered -= _numberToRemove;
+        holderTender.holderTotalTendered -= _numberToRemove;
+        overallTotalTendered -= _numberToRemove;
 
         // transfer these tokens from the holder to this contract
         securityToken.transfer(msg.sender, _numberToTender);
+    }
+
+    function finaliseTender(uint256 _batchSize) external isAfterEndTime {
+        // These requires are kind of redundant because of the while?
+        require(nextTenderToAssess < orderedHolders.length, "Finalisation is complete");
+        require(_batchSize > 0, "No batch size provided");
+        uint256 tendersProcessed = 0;
+        while(tendersProcessed < _batchSize && nextTenderToAssess < orderedHolders.length) {
+            if(totalRepurchased < totalToRepurchase) {
+                repurchaseTender();
+            } else {
+                returnTender();
+            }
+            nextTenderToAssess++;
+            tendersProcessed++;
+        }
+    }
+
+    function repurchaseTender() internal {
+        address holder = orderedHolders[nextTenderToAssess];
+        HolderTender storage holderTender = tenders[msg.sender];
+
+        // Assert the next to process isn't beyond the end of the array
+        assert(holderTender.nextToProcess < holderTender.tenderAmounts.length);
+
+        uint256 toRepurchase = min256(holderTender.tenderAmounts[nextToProcess], totalToRepurchase - totalRepurchased);
+        securityToken.transfer(issuer, toRepurchase);
+        paymentToken.transferFrom(issuer, holder, toRepurchase.mul(paymentPerSecurity));
+
+        totalRepurchased += toRepurchase;
+
+        // If not all of them could be repurchased, the rest must be returned
+        if (toRepurchase < holderTender.tenderAmounts[nextToProcess]) {
+            holderTender.tenderAmounts[nextToProcess] -= toRepurchase;
+            securityToken.transfer(holder, holderTender.tenderAmounts[nextToProcess]-toRepurchase);
+        }
+        holderTender.nextToProcess++;
+    }
+
+    function returnTender() internal {
+        address holder = orderedHolders[nextTenderToAssess];
+        HolderTender storage holderTender = tenders[msg.sender];
+
+        // Assert the next to process isn't beyond the end of the array
+        assert(holderTender.nextToProcess < holderTender.tenderAmounts.length);
+
+        // Return the tokens to the security holder
+        securityToken.transfer(holder, holderTender.tenderAmounts[nextToProcess]);
+        holderTender.nextToProcess++;
     }
 
     // TODO do we need to be able to specify who is entitled to a repurchase? Or if they start one are all holders entitled to one?

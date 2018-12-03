2943df386953be990bc93216da82308183e59775
Alice Henshaw <henshawalice@gmail.com>
2018-12-03 13:15:02 -0500

    merging constructor and initialisation

--- contracts/Redemptions/FullRedemption.sol
+++ contracts/Redemptions/FullRedemption.sol
@@ -12,23 +12,30 @@ contract FullRedemption is Ownable {
     RedeemableToken securityToken;
     address paymentOwner;
 
-    constructor (IERC20 _paymentToken, RedeemableToken _securityToken, address _paymentOwner) public {
+    constructor (
+        IERC20 _paymentToken,
+        RedeemableToken _securityToken,
+        address _paymentOwner,
+        uint256 _paymentPerSecurity
+    ) public {
+        require(address(_paymentToken) != 0, "Payment token contract required");
+        require(address(_securityToken) != 0, "Security token contract required");
+        require(address(_paymentOwner) != 0, "Payment owner address required");
+        require(_paymentPerSecurity > 0, "No payment amount provided");
+
+        uint256 totalPayment = securityToken.totalSupply() * _paymentPerSecurity;
+        require(
+            _paymentToken.allowance(paymentOwner, address(this)) >= totalPayment,
+            "Redemption contract does not have access to enough tokens"
+        );
         paymentToken = _paymentToken;
         securityToken = _securityToken;
         paymentOwner = _paymentOwner;
-    }
-
-    function initialiseRedemption(uint256 _paymentPerSecurity) public onlyOwner {
         paymentPerSecurity = _paymentPerSecurity;
-        uint256 numberOfSecurities = securityToken.totalSupply();
-        uint256 totalPayment = numberOfSecurities * _paymentPerSecurity;
-        require(
-            paymentToken.allowance(paymentOwner, address(this)) >= totalPayment,
-            "Redemption contract does not have access to enough tokens"
-        );
     }
 
     function redeemTokens(address[] _holders) public onlyOwner {
+        require(_holders.length > 0, "Empty array provided");
         for (uint256 i = 0; i < _holders.length; i++) {
             uint256 balance = securityToken.balanceOf(_holders[i]);
             securityToken.redeemAllTokens(_holders[i]);

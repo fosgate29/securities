98376479151b25790af99a1db504ed39e03128c5
Stuart Hunter <stuarth323@gmail.com>
2019-01-22 16:47:10 -0500

    more tests for voting

--- test/onChainVoting.js
+++ test/onChainVoting.js
@@ -1,5 +1,6 @@
 const Promise = require("bluebird");
 var CryptoJS = require("crypto-js");
+const { shouldFail, time } = require("openzeppelin-test-helpers");
 
 web3.eth = Promise.promisifyAll(web3.eth);
 
@@ -14,16 +15,17 @@ contract("OnChainVoting", accounts => {
     const holderOne = accounts[1];
     const holderTwo = accounts[2];
     const holderThree = accounts[3];
-    let date = (new Date("03/17/2020")).getTime();
-    let unixDate = Math.floor(date / 1000);
-    
+    const invalidHolder = accounts[4];
     
     beforeEach(async () => {
         securityToken = await SecurityToken.new(issuer, 1000000, { from: issuer });
         securityToken.transfer(holderOne, 1000, {from: issuer});
         securityToken.transfer(holderTwo, 500, {from: issuer});
-        securityToken.transfer(holderThree, 1200, {from: issuer});
-        onChainVoting = await OnChainVoting.new(securityToken.address, issuer, unixDate, {from: issuer});
+        securityToken.transfer(holderThree, 1500, {from: issuer});
+        let date = new Date();
+        date.setDate(date.getDate() + 14)
+        let endTime = Math.floor(date / 1000);
+        onChainVoting = await OnChainVoting.new(securityToken.address, issuer, endTime, {from: issuer});
     });
 
     it("should be owned by owner", async () => {
@@ -31,23 +33,34 @@ contract("OnChainVoting", accounts => {
         assert.strictEqual(currentOwner, issuer, "Contract is not owned by owner");
     });
 
-    it("HolderOne should have 1000 tokens", async () => {
+    it("should hold 1000 tokens for HolderOne", async () => {
         const balance = await securityToken.balanceOf.call(holderOne);
         assert.isTrue(balance.eq(new web3.utils.BN(1000)), "Contract is not owned by owner");
     });
 
-    it("HolderOne places their vote", async () => {
+    it("should allow HolderOne to place their vote", async () => {
         var ciphertext = CryptoJS.AES.encrypt('0,4,0', 'c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3');
         await onChainVoting.placeVote(ciphertext.toString(), { from: holderOne });
         const voteCount = await onChainVoting.submissionsCount.call();
         assert.isTrue(voteCount.eq(new web3.utils.BN(1)), "Vote was not succesfully placed");
     })
 
-    it("HolderTwo vote stored correctely", async () => {
+    it("should store HolderTwo's vote correctely", async () => {
         var ciphertext = CryptoJS.AES.encrypt('7,0,0', 'c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3');
         await onChainVoting.placeVote(ciphertext.toString(), { from: holderTwo });
         const voteSubmission = await onChainVoting.submissions.call(holderTwo);
         assert.strictEqual(voteSubmission, ciphertext.toString(), "Vote was not succesfully placed");
+    })
+
+    it("should not allow invalidHolder to vote", async () => {
+        var ciphertext = CryptoJS.AES.encrypt('1,0,0', 'c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3');
+        await shouldFail.reverting(onChainVoting.placeVote(ciphertext.toString(), { from: invalidHolder }));
+    })
+
+    it("should not allow someone to vote after the specified time", async () => {
+        var ciphertext = CryptoJS.AES.encrypt('1,0,0', 'c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3');
+        await time.increase(time.duration.years(3));
+        await shouldFail.reverting(onChainVoting.placeVote(ciphertext.toString(), { from: holderThree }))
     })
 
 });

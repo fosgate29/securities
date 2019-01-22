2557df702d66e15155b6f0518e0f9cd326504794
Stuart Hunter <stuarth323@gmail.com>
2019-01-22 15:02:43 -0500

    initial voting tests

--- test/onChainVoting.js
+++ test/onChainVoting.js
@@ -1,6 +1,5 @@
 const Promise = require("bluebird");
-// const increaseTime = require("./node_modules/openzeppelin-solidity/helpers/increaseTime");
-// const { BN, constants, expectEvent, shouldFail } = require('openzeppelin-test-helpers');
+var CryptoJS = require("crypto-js");
 
 web3.eth = Promise.promisifyAll(web3.eth);
 
@@ -25,7 +24,7 @@ contract("OnChainVoting", accounts => {
         securityToken.transfer(holderTwo, 500, {from: issuer});
         securityToken.transfer(holderThree, 1200, {from: issuer});
         onChainVoting = await OnChainVoting.new(securityToken.address, issuer, unixDate, {from: issuer});
-    })
+    });
 
     it("should be owned by owner", async () => {
         const currentOwner = await onChainVoting.owner.call();
@@ -34,8 +33,21 @@ contract("OnChainVoting", accounts => {
 
     it("HolderOne should have 1000 tokens", async () => {
         const balance = await securityToken.balanceOf.call(holderOne);
-        const tokenBalance = new web3.utils.BN(1000)
-        assert.isTrue(tokenBalance.eq(balance), "Contract is not owned by owner");
+        assert.isTrue(balance.eq(new web3.utils.BN(1000)), "Contract is not owned by owner");
+    });
+
+    it("HolderOne places their vote", async () => {
+        var ciphertext = CryptoJS.AES.encrypt('0,4,0', 'c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3');
+        await onChainVoting.placeVote(ciphertext.toString(), { from: holderOne });
+        const voteCount = await onChainVoting.submissionsCount.call();
+        assert.isTrue(voteCount.eq(new web3.utils.BN(1)), "Vote was not succesfully placed");
+    })
+
+    it("HolderTwo vote stored correctely", async () => {
+        var ciphertext = CryptoJS.AES.encrypt('7,0,0', 'c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3');
+        await onChainVoting.placeVote(ciphertext.toString(), { from: holderTwo });
+        const voteSubmission = await onChainVoting.submissions.call(holderTwo);
+        assert.strictEqual(voteSubmission, ciphertext.toString(), "Vote was not succesfully placed");
     })
 
 });
